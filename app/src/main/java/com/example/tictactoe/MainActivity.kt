ebde3a969549b6c9eed7d6cea8732276f1e77f5b
Shaimohammadayesha <shaikmohammadayesha@gmail.com>
2026-07-07 00:27:21 +0530

    replaced cell txt with icon and changed ui

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,22 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -27,6 +29,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,77 +56,85 @@ class MainActivity : ComponentActivity() {
 fun TicTacToe(
     modifier: Modifier = Modifier
 ){
-
-    val buttonTexts = remember { mutableStateListOf("", "", "", "", "", "", "", "", "") }
+    val playerX = R.drawable.icons8_x_64
+    val playerO = R.drawable.icons8_o_64
+    val gameCells = remember { mutableStateListOf("", "", "", "", "", "", "", "", "") }
     var currentPlayer by remember { mutableStateOf("X") }
     var winner by remember { mutableStateOf("") }
-    var btnCount by remember { mutableStateOf(0) }
+    var cellsPlayed by remember { mutableIntStateOf(0) }
+    var cellsEnabled by remember { mutableStateOf(true) }
+    val onResetClick: () -> Unit = {
+        for (i in gameCells.indices){
+            gameCells[i] = ""
+        }
+        
+        cellsPlayed = 0
+        winner = ""
+        cellsEnabled = true
+
+    }
     val onClickAction: (Int) -> Unit =  { index ->
         println("Button $index clicked")
-        while (buttonTexts[index].isEmpty()) {
-            buttonTexts[index] = currentPlayer
-        }
-        btnCount++
-        if (checkForWin(currentPlayer, buttonTexts)){
-           winner = currentPlayer
+        if (gameCells[index].isEmpty()) {
+            gameCells[index] = currentPlayer
+            cellsPlayed++
+            if (checkForWin(currentPlayer, gameCells)){
+                winner = currentPlayer
+                cellsEnabled = false
+            }
+            currentPlayer = if (currentPlayer == "X") "O" else "X"
 
         }
-        currentPlayer = if (currentPlayer == "X") "O" else "X"
+
     }
-    if (btnCount == 9 || winner != ""){
-        Box(
-            modifier = Modifier
-        ){
-            Text(
-                text = if ( winner != "") "$winner wins" else "Draw",
-                fontSize = 17.sp,
 
-                color = Color.White,
+    if (cellsPlayed == 9 || winner != "" ){
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Text(
+                text = if ( winner != "") "🎉 $winner wins" else "Draw",
+                fontSize = 20.sp,
+                color = Color.Green,
+                textAlign = TextAlign.Center,
                 modifier = Modifier
                     .padding(top = 15.dp)
-
                     .fillMaxWidth()
-                    .background(color = Color.Gray)
+
 
             )
+
+//            Reset( "Replay", modifier = Modifier) {
+//                onResetClick()
+//            }
         }
     }
+
     Box(
         modifier = Modifier
 
             .padding(16.dp)
     ){
-        Board(modifier, onClickAction, buttonTexts)
-        Button(
-            onClick =
-                {
-                    for (i in 0 until buttonTexts.size){
-                        buttonTexts[i] = ""
-                    }
-                    btnCount = 0
-                    winner = ""
-
-                },
+        Board(modifier, onClickAction, gameCells, cellsEnabled, playerX, playerO)
+        Reset( "Reset",
             modifier = Modifier
-                .padding(bottom = 40.dp)
-                .align(Alignment.BottomCenter)
+            .align(Alignment.BottomCenter)
         ){
-            Text(
-                text = "Restart",
-                fontSize = 18.sp,
-                modifier = Modifier
-                    .padding(12.dp)
-            )
+            onResetClick()
         }
+
     }
 
 }
 
 
 @Composable
-fun Board(modifier: Modifier, onClickAction: (Int) -> Unit, buttonTexts: List<String>) {
+fun Board(modifier: Modifier, onClickAction: (Int) -> Unit, gameCells: List<String>, btnsEnabeled: Boolean, playerX: Int, playerO: Int) {
     Column(
-
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()
@@ -134,7 +146,7 @@ fun Board(modifier: Modifier, onClickAction: (Int) -> Unit, buttonTexts: List<St
             ) {
                 repeat(3){
                         col ->
-                    SingleCell(onClickAction, (row * 3 ) + col, buttonTexts)
+                    SingleCell(onClickAction, (row * 3 ) + col, gameCells, btnsEnabeled, playerX, playerO)
                 }
             }
 
@@ -143,17 +155,47 @@ fun Board(modifier: Modifier, onClickAction: (Int) -> Unit, buttonTexts: List<St
 }
 
 @Composable
-fun SingleCell(onClickAction: (Int) -> Unit, buttonIndex: Int, buttonTexts: List<String>) {
+fun SingleCell(onClickAction: (Int) -> Unit, buttonIndex: Int, gameCells: List<String>, btnsEnabeled: Boolean, playerX: Int, playerO: Int) {
     Button(
         onClick = { onClickAction(buttonIndex) },
-        colors = ButtonDefaults.buttonColors(containerColor = Color.Gray),
-        shape = RoundedCornerShape(8.dp),
-//        enabled = checkForWin,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Gray,
+            disabledContainerColor = Color.LightGray
+        ),
+        shape = RoundedCornerShape(5.dp),
+        enabled = btnsEnabeled && gameCells[buttonIndex].isEmpty(),
         modifier = Modifier
             .padding(2.dp)
-            .size(90.dp)
-        ) {
-        Text(text = buttonTexts[buttonIndex])
+            .size(95.dp)
+    ) {
+        if (gameCells[buttonIndex].isNotEmpty()){
+            val playerImg = if (gameCells[buttonIndex] == "X") playerX else playerO
+            val playerColor = if (gameCells[buttonIndex] == "X") Color.Red else Color.Blue
+            Icon(
+                painter = painterResource(id = playerImg),
+                tint = playerColor,
+//                tint = Color.Unspecified,
+                contentDescription = "Player ${gameCells[buttonIndex]}",
+            )
+        }
+    }
+}
+
+@Composable
+fun Reset(restart: String, modifier: Modifier,onResetClick: () -> Unit){
+    Button(
+        onClick = { onResetClick()},
+        shape = RoundedCornerShape(5.dp),
+        modifier = modifier
+            .padding(bottom = 40.dp)
+            .fillMaxWidth()
+    ){
+        Text(
+            text = restart,
+            fontSize = 18.sp,
+            modifier = Modifier
+                .padding(12.dp)
+        )
     }
 }
 
@@ -207,6 +249,6 @@ private fun diagonalCheck(buttonTexts: List<String>, currentPlayer: String): Boo
 @Composable
 fun GreetingPreview() {
     TicTacToeTheme {
-        TicTacToe()
+        TicTacToe(Modifier.fillMaxSize())
     }
 }
